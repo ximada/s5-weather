ab6fdfeb5cf8b5856e0da506c44d363bf6394853
ximada <witch_ada@hotmail.com>
2018-03-08 00:39:19 -0600

    funcion para imprimir los dias de la semana

--- js/app.js
+++ js/app.js
@@ -1,32 +1,40 @@
 //  Obteniendo datos de  Geolocation
  const apiGeolocation = () =>{
-     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCzE3lK9K-zPt7BGZKsKs6CZKuV1Ec0XEs")
-         .then(function(response){
+     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAyw9GBOW1m7Brs0yL8cfqfbCWM0rAx_hI")
+         .then(response=> {
              return response.json();
          })
-         .then(function(data){
-             console.log('Request succesful',data)
-
+         .then(dataGeo =>{
+             console.log('Request succesful',dataGeo)
+             locationCountry(dataGeo);
          })
-         .catch(function(error){
+         .catch(error=>{
              console.log('Request failed', error)
          })
 }
 apiGeolocation();
 
- 
- const apiLoad = () =>{
-     fetch("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2bd104aaaf24feedc7378c5d0cc0588/23.6345005,-102.5527878")
-         .then(function (response) {
+const locationCountry = dataGeo =>{
+    const lat = dataGeo;
+
+}
+// Llamando elementos del HTML
+const weatherTodayContainer = document.getElementById('weather-today-container');
+const weatherForecastContainer = document.getElementById('weather-forecast-container');
+
+//  Obtenido data de API darksky
+ const apiLoad = () => {
+         fetch("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/a2bd104aaaf24feedc7378c5d0cc0588/23.6345005,-102.5527878")
+         .then(function (response){
              return response.json();
          })
-         .then(function (data) {
+         .then(function (data){
              console.log('Request succesful', data);
               reponsePronostic(data);
              
 
          })
-         .catch(function (error) {
+         .catch(function (error){
              console.log('Request failed', error)
          });
  }
@@ -44,13 +52,41 @@ apiGeolocation();
      //console.log(atmosphericPressure);
     //  
      const dailyWeather = data.daily.data;
-      console.log(dailyWeather)
+      //console.log(dailyWeather)
 
-     paintCurretlyWeather(iconWeather, windSpeed, humidityWeather, uvIndex, atmosphericPressure)
+     paintCurretlyWeather(iconWeather, windSpeed, humidityWeather, uvIndex, atmosphericPressure, dailyWeather);
+     printForecastWeather(dailyWeather);
  }
  
 
 // printing data on the screen
-const paintCurretlyWeather = (iconWeather, windSpeed, humidityWeather, uvIndex, atmosphericPressure) =>{
+const paintCurretlyWeather = (iconWeather, windSpeed, humidityWeather, uvIndex, atmosphericPressure,dailyWeather) =>{
+    const weatherTodayContainer = document.getElementById('weather-today-container');
+    
+
+             let template =`<div>
+                             <img src="./assets/images/icon.jpg" alt ="image" style="width:20%;">
+                             <p>Wind: ${windSpeed} m/s</p>
+                             <p>Humidity: ${humidityWeather} %</p>
+                             <p>UV Index: ${uvIndex}</p>
+                             <p>Pressure: ${atmosphericPressure} hPa</p>
+                            </div>`
+    weatherTodayContainer.innerHTML = template;
+     
 
 }
+
+const printForecastWeather = (dailyWeather)=>{
+    const weatherForecastContainer = document.getElementById('weather-forecast-container');
+      
+    dailyWeather.forEach(days=>{
+        let templateForecast = `<div> 
+                                  <p>${unixDateToCurrentDate(days.time)}</p>
+                                  <img src="./assets/images/icon-week.png" alt ="image" style="width:20%;">
+                                  <p>${days.summary}</p>
+                                  <p>Temperature-high: ${days.temperatureHigh} and Temperature-min: ${days.temperatureMin}</p>
+                                </div>`
+        weatherForecastContainer.insertAdjacentHTML('beforeEnd', templateForecast);
+    })
+}
+const unixDateToCurrentDate = (unixNumber) => new Date(unixNumber * 1000).toLocaleString('en-US', { weekday: 'long' });
